231af7f7dae068edd34128c913f03f377213e21a
Chengheng Li <li471115381@gmail.com>
2024-10-18 12:19:24 +0200

    add volumnes to front and back

--- backend/app/database/config.py
+++ backend/app/database/config.py
@@ -4,8 +4,14 @@ from sqlalchemy.ext.declarative import declarative_base
 
 URL_DATABASE = "postgresql://user:password@postgres:5432/Echolink"
 
+URL_DATABASE_LOCAL = "postgresql://user:password@localhost:5432/Echolink"
+
+
 engine = create_engine(URL_DATABASE)
+
+#engine = create_engine(URL_DATABASE_LOCAL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
