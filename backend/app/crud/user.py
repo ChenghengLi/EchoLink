3ebf999cec4bcff94b66fb22873d95c1118aed31
Diego2811 <diegogelida0@gmail.com>
2024-10-18 18:52:57 +0200

    Create user function

---
+++ backend/app/crud/user.py
@@ -0,0 +1,14 @@
+""" User related CRUD methods """
+from sqlalchemy.orm import Session
+from app.core.models import User, UserInput
+from app.core.security import get_password_hash
+
+# User creation
+def create_user(db: Session, user_input: UserInput) -> User:
+    db_user = User.model_validate(
+        user_input, update={"hashed_password": get_password_hash(user_input.password)}
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
