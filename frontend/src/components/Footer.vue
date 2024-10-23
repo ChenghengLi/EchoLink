78e8b6c231d555d5c59e3c2155bcd45d3db672d2
griberto7 <griberto7@alumnes.ub.edu>
2024-10-23 13:13:13 +0200

    footer done

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -2,66 +2,19 @@
   <footer class="footer-two section bg-img">
     <div class="container">
       <div class="row items-gap-two">
-        <div class="col-12 col-sm-6 col-xl-4 col-xxl-5">
-          <div class="footer-two__single bt wow fadeInUp" data-wow-duration="600ms">
+        <!--<div class="col-12 col-sm-6 col-xl-6 col-xxl-5">-->
+        <div class="col-10 col-sm-10 col-xl-10 col-xxl-10">
+          <!--<div class="footer-two__single bt wow fadeInUp" data-wow-duration="600ms">-->
+          <div class="footer-two__single wow fadeInUp" data-wow-duration="600ms">
             <router-link to="/" class="logo">
-              <img src="" alt="Image" />
+              <img src="../assets/logo.png" alt="Image" />
             </router-link>
             <p>
               Copyright &copy;
               <span id="copyYear"></span>
-              <router-link to="/">  EchoLink</router-link>
+              <a href="/" aria-current="page" class="router-link-exact-active router-link-active">EchoLink</a>
               . All Rights Reserved.
             </p>
-            <!--<div class="social">
-              <a href="https://www.facebook.com" aria-label="social media">
-                <i class="fa-brands fa-facebook-f"></i>
-              </a>
-              <a href="https://www.linkedin.com" aria-label="social media">
-                <i class="fa-brands fa-linkedin-in"></i>
-              </a>
-              <a href="https://www.instagram.com" aria-label="social media">
-                <i class="fa-brands fa-instagram"></i>
-              </a>
-            </div>--->
-          </div>
-        </div>
-        <div class="col-12 col-sm-6 col-xl-2 col-xxl-2">
-          <div class="footer-two__single wow fadeInUp" data-wow-duration="600ms" data-wow-delay="200ms">
-            <h5 class="h5">Resources</h5>
-            <ul>
-              <li>
-                <a href="/">Link</a>
-              </li>
-           
-            </ul>
-          </div>
-        </div>
-        <div class="col-12 col-sm-6 col-xl-2 col-xxl-2">
-          <div class="footer-two__single wow fadeInUp" data-wow-duration="600ms" data-wow-delay="400ms">
-            <h5 class="h5">Activities</h5>
-            <ul>
-              <li>
-                <a href="javascript:void(0)">Upcoming</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div class="col-12 col-sm-6 col-xl-4 col-xxl-3">
-          <div class="footer-two__single wow fadeInUp" data-wow-duration="600ms" data-wow-delay="600ms">
-            <div class="footer-two__form">
-              <h6 class="h6">Subscribe to Our Newsletter for Events & Activities!</h6>
-              <form @submit="subscribeToNewsletter" method="post">
-                <div class="mail-group">
-                  <input type="email" name="subscribe-news" id="subscribeNews" placeholder="Enter your email"
-                    required />
-                  <button type="submit" aria-label="subscribe to our newsletter">
-                    <i class="fa-regular fa-paper-plane"></i>
-                  </button>
-                </div>
-              </form>
-
-            </div>
           </div>
         </div>
       </div>
@@ -71,7 +24,6 @@
 
 <script>
 
-
 export default {
   name: "Footer2",
   mounted() {
@@ -80,17 +32,20 @@ export default {
   methods: {
    
     
-  }
+  },
 };
 </script>
 
 
 <style>
+/*
 .footer-two {
   background-image: url("~@/assets/images/footer-two-bg.png");
 }
 
 .home-two-light .footer-two {
-  background-image: url("~@/assets/images/footer-two-bg-light.png");
-}
+  background-image: url("~@/assets/images/footer-two-bg.png");
+}*/
 </style>
+
+
