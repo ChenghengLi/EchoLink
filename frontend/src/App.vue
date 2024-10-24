b4a3852fc55120cd1cfaf93962bcfdf948d4e89b
Chengheng Li <li471115381@gmail.com>
2024-10-24 09:55:57 +0200

    Hotfix: Frontend 500 (Internal Server Error), change image from Nginx to Caddy and add CORS config at the backend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,65 +5,81 @@
 </template>
 
 <script>
-  import 'wowjs/css/libs/animate.css';
-  import 'bootstrap/dist/css/bootstrap.css';
-  import './assets/vendor/font-awesome/css/all.min.css'
-  import './assets/vendor/bootstrap/js/bootstrap.bundle.min.js'
-  import './assets/css/main.css'
-  import './assets/vendor/glyphter/css/Glyphter.css'
+import axios from 'axios';
+import 'wowjs/css/libs/animate.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import './assets/vendor/font-awesome/css/all.min.css';
+import './assets/vendor/bootstrap/js/bootstrap.bundle.min.js';
+import './assets/css/main.css';
+import './assets/vendor/glyphter/css/Glyphter.css';
 
-  export default {
-    name: 'App'
+
+// Move the test conexions method to tests when tests created
+export default {
+  name: 'App',
+  created() {
+    this.logMessage();
+  },
+  methods: {
+    async logMessage() {
+      try {
+        const response = await axios.post('http://localhost:8000/test/test');
+        console.log(response.data); // Log the message from the backend
+      } catch (error) {
+        console.error('Error fetching test message:', error);
+      }
+    }
   }
+};
 </script>
 
 <style>
-  .voice__slider-container {
-        overflow: hidden;
-        user-select: none;
-    }
+.voice__slider-container {
+  overflow: hidden;
+  user-select: none;
+}
 
-    .cmn-pagination {
-        display: flex;
-        gap: 6px;
-        align-items: center;
-        justify-content: flex-end;
-    }
+.cmn-pagination {
+  display: flex;
+  gap: 6px;
+  align-items: center;
+  justify-content: flex-end;
+}
 
-    .cmn-pagination .swiper-pagination-bullet {
-        width: 28px;
-        height: 28px;
-        background-color: transparent;
-        border: 1px solid transparent;
-        position: relative;
-        transition: all 0.4s ease-in-out;
-        border-radius: 50%;
-        opacity: 1;
-    }
+.cmn-pagination .swiper-pagination-bullet {
+  width: 28px;
+  height: 28px;
+  background-color: transparent;
+  border: 1px solid transparent;
+  position: relative;
+  transition: all 0.4s ease-in-out;
+  border-radius: 50%;
+  opacity: 1;
+}
 
-    .cmn-pagination .swiper-pagination-bullet::before {
-        content: "";
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        width: 10px;
-        height: 10px;
-        background-color: #8fffec;
-        border-radius: 50%;
-    }
+.cmn-pagination .swiper-pagination-bullet::before {
+  content: "";
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 10px;
+  height: 10px;
+  background-color: #8fffec;
+  border-radius: 50%;
+}
 
-    .home-light .cmn-pagination .swiper-pagination-bullet::before {
-        background-color: #4569e7;
-    }
+.home-light .cmn-pagination .swiper-pagination-bullet::before {
+  background-color: #4569e7;
+}
 
-    .cmn-pagination .swiper-pagination-bullet-active {
-        background-color: transparent;
-        border: 1px solid #8fffec;
-    }
+.cmn-pagination .swiper-pagination-bullet-active {
+  background-color: transparent;
+  border: 1px solid #8fffec;
+}
 
-    .home-light .cmn-pagination .swiper-pagination-bullet-active {
-        background-color: transparent;
-        border: 1px solid #4569e7;
-    }
+.home-light .cmn-pagination .swiper-pagination-bullet-active {
+  background-color: transparent;
+  border: 1px solid #4569e7;
+}
 </style>
