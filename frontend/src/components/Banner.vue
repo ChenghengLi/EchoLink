ad6234620abe798b2d0febceef7dc85b6d49b621
griberto7 <griberto7@alumnes.ub.edu>
2024-10-24 10:08:49 +0200

    pag dummy v1

--- frontend/src/components/Banner.vue
+++ frontend/src/components/Banner.vue
@@ -11,21 +11,87 @@
                             </p>
                             <h1 class="h1">Music For Everyone</h1>
                             <div class="section__content-cta">
-                                <button @click="learnMore" class="btn btn--primary">
+                                <a href="#learnMore" class="btn btn--primary">Learn More</a>
+                                <!--<button class="btn btn--primary">
                                     Learn More
-                                </button>
+                                </button>-->
                             </div>
                         </div>
                     </div>
                     <div class="col-12 col-lg-6">
                         <div class="banner-one__thumb text-start text-lg-end">
-                            <img src="" alt="Image" class="wow fadeInUp">
+                            <img src="../images/img_dummy1.png" alt="Image" class="wow fadeInUp">
                         </div>
                     </div>
                 </div>
             </div>
         </section>
     </div>
+    <section class="section broadcast pb-0">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 fix-scroll">
+                    <div data-wow-duration="600ms" class="broadcast__inner wow fadeInUp" style="visibility: visible; animation-duration: 600ms;">
+                        <div class="row align-items-center itemps-gap-two">
+                            <div class="col-12 col-xl-8 col-xxl-7">
+                                <div class="section__content broadcast__inner-content" id="learnMore">
+                                    <h2 class="h2">What is EchoLink</h2>
+                                    <div class="paragraph">
+                                        <p>
+                                            PROVA PROVA PROVA PROVA
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 col-xl-4 col-xxl-5">
+                                <div class="broadcast__thumb text-xl-center">
+                                    <img src="../images/img_dummy1.png" alt="Image">
+                                </div>
+                            </div>
+                        </div>
+                    </div> 
+                    <div data-wow-duration="600ms" class="broadcast__inner wow fadeInUp" style="visibility: visible; animation-duration: 600ms;">
+                        <div class="row align-items-center itemps-gap-two">
+                            <div class="col-12 col-xl-4 col-xxl-5">
+                                <div class="broadcast__thumb text-xl-center">
+                                    <img src="../images/img_dummy1.png" alt="Image">
+                                </div>
+                            </div>
+                            <div class="col-12 col-xl-8 col-xxl-7">
+                                <div class="section__content broadcast__inner-content">
+                                    <h2 class="h2">What is EchoLink</h2>
+                                    <div class="paragraph">
+                                        <p>
+                                            PROVA PROVA PROVA PROVA
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div data-wow-duration="600ms" class="broadcast__inner wow fadeInUp" style="visibility: visible; animation-duration: 600ms;">
+                        <div class="row align-items-center itemps-gap-two">
+                            <div class="col-12 col-xl-8 col-xxl-7">
+                                <div class="section__content broadcast__inner-content">
+                                    <h2 class="h2">What is EchoLink</h2>
+                                    <div class="paragraph">
+                                        <p>
+                                            PROVA PROVA PROVA PROVA
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 col-xl-4 col-xxl-5">
+                                <div class="broadcast__thumb text-xl-center">
+                                    <img src="../images/img_dummy1.png" alt="Image">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
