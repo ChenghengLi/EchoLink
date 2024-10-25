097b03e73dcb6a5d1671a5b1206d0b1039595348
Kamil <lekamilthemage@gmail.com>
2024-10-25 08:46:09 +0200

    Frontend: fix user endpoint

--- frontend/src/services/user.js
+++ frontend/src/services/user.js
@@ -3,7 +3,7 @@ import axios from './baseAxiosClient';
 class UserService {
     async registerAccount(username, email, password) {
         try {
-            const response = await axios.post('/user/', {
+            const response = await axios.post('/users/user', {
                 'username': username,
                 'email': email,
                 'password': password,
