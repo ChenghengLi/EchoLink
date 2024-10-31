e7a032193a929498ac43af0dc3ef9711f1215638
Diego <diegogelida0@gmail.com>
2024-10-31 14:43:45 +0100

    #23: POST endpoint for login

--- backend/app/routes/login.py
+++ backend/app/routes/login.py
@@ -2,12 +2,16 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from core.config import get_db
 from models.user import UserLogin, Token
+from crud.user import authenticate
 
 router = APIRouter()
 
 @router.post("/")
-def login_access_token(user_login: UserLogin, db: Session = Depends(get_db)):
+def login_access_token(user_login: UserLogin, db: Session = Depends(get_db)) -> Token:
     """
     User login, get an access token for future requests
     """
-    pass
+    try:
+        return Token(access_token=authenticate(db, user_login))
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
