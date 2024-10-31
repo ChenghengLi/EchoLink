b9950d4595fe5f541089236fe849cf08fdea6972
Diego <diegogelida0@gmail.com>
2024-10-31 09:35:18 +0100

    #24 JWT creation (secrets in .env)

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,4 +1,8 @@
 """ Security related methods """
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
+import os
+from jose import jwt
 from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -8,3 +12,18 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 
 def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Data from .env file
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+EXPIRE_DELTA = timedelta(days=int(os.getenv("ACCESS_TOKEN_EXPIRE_DAYS")))
+
+# Function to create JWT token
+def create_access_token(subject) -> str:
+    expire = datetime.utcnow() + EXPIRE_DELTA
+    to_encode = {"exp": expire, "sub": str(subject)}
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
