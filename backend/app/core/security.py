d13c5a348c610c74ae76b60b0185f33376099dee
Diego <diegogelida0@gmail.com>
2024-11-04 14:18:05 +0100

    #26 check_token endpoint

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,8 +1,15 @@
 """ Security related methods """
 from datetime import datetime, timedelta
+from typing import Annotated
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from pydantic import ValidationError
+from pytest import Session
+from core.config import get_db
+from fastapi import Depends, HTTPException, status
+from models.user import User, TokenPayload
 from dotenv import load_dotenv
 import os
-from jose import jwt
+from jose import jwt, JWTError, ExpiredSignatureError
 from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -27,3 +34,30 @@ def create_access_token(subject) -> str:
     to_encode = {"exp": expire, "sub": str(subject)}
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+# Security scheme for token authentication
+security = HTTPBearer()
+
+# Dependency to get user and verify token
+def get_current_user(db: Session = Depends(get_db), 
+                     credentials: HTTPAuthorizationCredentials = Depends(security)) -> User:
+    token = credentials.credentials
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        token_data = TokenPayload(**payload)
+    except ExpiredSignatureError:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Token expired") 
+    except (JWTError, ValidationError):
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Could not validate credentials")
+    user = db.get(User, token_data.sub)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User not found")
+    
+    # JWT valid for the user, check if it is the one in the database
+    if user.token != token:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+    
+    return user
+
+# User annotation
+CurrentUser = Annotated[User, Depends(get_current_user)]
--- backend/app/routes/login.py
+++ backend/app/routes/login.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy.orm import Session
+from pytest import Session
 from core.config import get_db
+from core.security import CurrentUser
 from models.user import UserLogin, Token
 from crud.user import authenticate
 
@@ -15,3 +16,10 @@ def login_access_token(user_login: UserLogin, db: Session = Depends(get_db)) -> 
         return Token(access_token=authenticate(db, user_login))
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
+
+@router.get("/check_token/")
+def login_check_token(user: CurrentUser):
+    """
+    Check access token is valid
+    """
+    pass
