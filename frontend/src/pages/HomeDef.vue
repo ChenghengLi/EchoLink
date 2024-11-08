cc60c5be2209ccccb2aa8d85d9e5523e0d45eade
griberto7 <griberto7@alumnes.ub.edu>
2024-11-08 22:53:34 +0100

    Visual improvements #36 Add scroll-to-top button functionality

--- frontend/src/pages/HomeDef.vue
+++ frontend/src/pages/HomeDef.vue
@@ -10,6 +10,11 @@
 			<Broadcast />
 			<Footer class="footer-light mx-10" />
 		</div>
+		<div class="progress-wrap active-progress" @click="scrollToTop">
+			<svg width="100%" height="100%" viewBox="-1 -1 102 102" class="progress-circle svg-content">
+				<path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" style="transition: stroke-dashoffset 10ms linear; stroke-dasharray: 307.919, 307.919; stroke-dashoffset: 178,377;"></path>
+			</svg>
+		</div>
 
 	</div>
 </template>
@@ -27,11 +32,31 @@ export default {
 		Banner,
 		Broadcast,
 
+	}, mounted(){
+		const progressPath = this.$el.querySelector(".progress-wrap path");
+  		const pathLength = 307.919;
+
+		const updateProgress = () => {
+			const scrollTop = window.scrollY;
+			const docHeight = document.documentElement.scrollHeight - window.innerHeight;
+			const progress = (scrollTop / docHeight) * 100;
+
+			const offset = pathLength - (pathLength * progress) / 100;
+			progressPath.style.strokeDashoffset = offset;
+		};
+
+		window.addEventListener("scroll", updateProgress);
+		updateProgress();
+	}, beforeDestroy(){
+		window.removeEventListener("scroll", this.updateProgress);
 	}, methods: {
 		scrollToBroadcast() {
 			this.$nextTick(() => {
 				this.$refs.broadcastComponent.$el.scrollIntoView({ behavior: 'smooth' });
 			});
+		},
+		scrollToTop(){
+			window.scrollTo({ top: 0, behavior: 'smooth' });
 		}
 	}
 };
